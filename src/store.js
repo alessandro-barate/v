461796217a019e82ee56f4237b04cd0486855736
alessandro-barate <alebarat@gmail.com>
2024-06-05 19:31:27 +0100

    [UPD]

--- src/store.js
+++ src/store.js
@@ -3,6 +3,6 @@ import {reactive} from 'vue';
 export const store = reactive({
     endpoints: {
     baseUrl: 'https://api.themoviedb.org/3/',
-    myKey: 'bda3e142a7908a93f44deceb31c6bc17',
+    myCode: 'bda3e142a7908a93f44deceb31c6bc17',
     }
 });
