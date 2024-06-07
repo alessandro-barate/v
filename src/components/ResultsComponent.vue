40bbe2b6eb8fdad7662bf3ac4fad1d2b607735f8
alessandro-barate <alebarat@gmail.com>
2024-06-07 10:02:35 +0100

    [UPD] Other flags added

--- src/components/ResultsComponent.vue
+++ src/components/ResultsComponent.vue
@@ -17,8 +17,14 @@ export default {
         <li>{{ movie.title }}</li>
         <li>{{ movie.original_title }}</li>
         <li>
-            <img v-if="movie.original_language === 'it'" src="../assets/img/italy-flag.png" alt="Bandiera Italia" class="flag">
-            <img v-else-if="movie.original_language === 'en'" src="../assets/img/uk-flag.svg" alt="Bandiera Regno Unito" class="flag">
+            <img v-if="movie.original_language === 'it'" src="../assets/img/flags/italy-flag.png" alt="Bandiera Italia" class="flag">
+            <img v-else-if="movie.original_language === 'en'" src="../assets/img/flags/uk-flag.svg" alt="Bandiera Regno Unito" class="flag">
+            <img v-else-if="movie.original_language === 'de'" src="../assets/img/flags/germany-flag.png" alt="Bandiera Germania" class="flag">
+            <img v-else-if="movie.original_language === 'fr'" src="../assets/img/flags/france-flag.svg" alt="Bandiera Francia" class="flag">
+            <img v-else-if="movie.original_language === 'es'" src="../assets/img/flags/spain-flag.png" alt="Bandiera Spagna" class="flag">
+            <img v-else-if="movie.original_language === 'ja'" src="../assets/img/flags/japan-flag.png" alt="Bandiera Giappone" class="flag">
+            <img v-else-if="movie.original_language === 'ch'" src="../assets/img/flags/china-flag.png" alt="Bandiera Cina" class="flag">
+            <img v-else-if="movie.original_language === 'zh'" src="../assets/img/flags/switzerland-flag.png" alt="Bandiera Svizzera" class="flag">
             <p v-else>{{ movie.original_language }}</p>
         </li>
         <li>{{ movie.vote_average }}</li>
@@ -31,8 +37,14 @@ export default {
         <li>{{ movie.name }}</li>
         <li>{{ movie.original_name }}</li>
         <li>
-            <img v-if="movie.original_language === 'it'" src="../assets/img/italy-flag.png" alt="Bandiera Italia" class="flag">
-            <img v-else-if="movie.original_language === 'en'" src="../assets/img/uk-flag.svg" alt="Bandiera Regno Unito" class="flag">
+            <img v-if="movie.original_language === 'it'" src="../assets/img/flags/italy-flag.png" alt="Bandiera Italia" class="flag">
+            <img v-else-if="movie.original_language === 'en'" src="../assets/img/flags/uk-flag.svg" alt="Bandiera Regno Unito" class="flag">
+            <img v-else-if="movie.original_language === 'de'" src="../assets/img/flags/germany-flag.png" alt="Bandiera Germania" class="flag">
+            <img v-else-if="movie.original_language === 'fr'" src="../assets/img/flags/france-flag.svg" alt="Bandiera Francia" class="flag">
+            <img v-else-if="movie.original_language === 'es'" src="../assets/img/flags/spain-flag.png" alt="Bandiera Spagna" class="flag">
+            <img v-else-if="movie.original_language === 'ja'" src="../assets/img/flags/japan-flag.png" alt="Bandiera Giappone" class="flag">
+            <img v-else-if="movie.original_language === 'ch'" src="../assets/img/flags/china-flag.png" alt="Bandiera Cina" class="flag">
+            <img v-else-if="movie.original_language === 'zh'" src="../assets/img/flags/switzerland-flag.png" alt="Bandiera Svizzera" class="flag">
         </li>
         <li>{{ movie.vote_average }}</li>
     </ul>
