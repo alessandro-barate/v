cf6bf855b716a65644e099abb5dded13f446fd3d
alessandro-barate <alebarat@gmail.com>
2024-06-07 09:12:35 +0100

    [UPD] Little refactor - ResultsComponent created

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -2,10 +2,12 @@
 import axios from 'axios';
 import { store } from '../store.js';
 import SearchComponent from '../components/SearchComponent.vue';
+import ResultsComponent from '../components/ResultsComponent.vue';
 export default {
     name: 'MainComponent',
     components: {
        SearchComponent,
+       ResultsComponent,
     },
 
     data(){
@@ -43,16 +45,11 @@ export default {
             console.log(this.store.tvResults);
             });
 
-
-            
             this.store.loading = false;
-
-            
 
             this.store.endpoints.query = '';
         },
-    },
-    
+    },   
 };
 </script>
 
@@ -70,27 +67,7 @@ export default {
                             
                         </div>
                         <div class="lower-space">
-                            <ul v-for="movie in this.store.movieResults">
-                                <li>{{ movie.title }}</li>
-                                <li>{{ movie.original_title }}</li>
-                                <li>
-                                    <img v-if="movie.original_language === 'it'" src="../assets/img/italy-flag.png" alt="Bandiera Italia" class="flag">
-                                    <img v-else-if="movie.original_language === 'en'" src="../assets/img/uk-flag.svg" alt="Bandiera Regno Unito" class="flag">
-                                    <p v-else>{{ movie.original_language }}</p>
-                                </li>
-                                <li>{{ movie.vote_average }}</li>
-                            </ul>
-                                <hr>
-                                <hr>
-                            <ul v-for="movie in this.store.tvResults">
-                                <li>{{ movie.name }}</li>
-                                <li>{{ movie.original_name }}</li>
-                                <li>
-                                    <img v-if="movie.original_language === 'it'" src="../assets/img/italy-flag.png" alt="Bandiera Italia" class="flag">
-                                    <img v-else-if="movie.original_language === 'en'" src="../assets/img/uk-flag.svg" alt="Bandiera Regno Unito" class="flag">
-                                </li>
-                                <li>{{ movie.vote_average }}</li>
-                            </ul>
+                            <ResultsComponent/>
                         </div>
                     </div>
                 </div>
---
+++ src/components/ResultsComponent.vue
@@ -0,0 +1,39 @@
+<script>
+import { store } from '../store.js';
+export default {
+    name: 'ResultsComponent',
+
+    data(){
+        return {
+            store,
+        };
+    },
+    
+};
+</script>
+
+<template>
+    <ul v-for="movie in this.store.movieResults">
+        <li>{{ movie.title }}</li>
+        <li>{{ movie.original_title }}</li>
+        <li>
+            <img v-if="movie.original_language === 'it'" src="../assets/img/italy-flag.png" alt="Bandiera Italia" class="flag">
+            <img v-else-if="movie.original_language === 'en'" src="../assets/img/uk-flag.svg" alt="Bandiera Regno Unito" class="flag">
+            <p v-else>{{ movie.original_language }}</p>
+        </li>
+        <li>{{ movie.vote_average }}</li>
+    </ul>
+
+    <hr>
+    <hr>
+
+    <ul v-for="movie in this.store.tvResults">
+        <li>{{ movie.name }}</li>
+        <li>{{ movie.original_name }}</li>
+        <li>
+            <img v-if="movie.original_language === 'it'" src="../assets/img/italy-flag.png" alt="Bandiera Italia" class="flag">
+            <img v-else-if="movie.original_language === 'en'" src="../assets/img/uk-flag.svg" alt="Bandiera Regno Unito" class="flag">
+        </li>
+        <li>{{ movie.vote_average }}</li>
+    </ul>
+</template>
