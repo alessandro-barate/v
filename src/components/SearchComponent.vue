5f8f0afe45948ac6ebb07aa83a3df880a2b0a875
alessandro-barate <alebarat@gmail.com>
2024-06-08 14:56:05 +0100

    [UPD] Added a second list to the header

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -14,6 +14,20 @@ export default {
 
 <template>
     <div class="search-section">
+        <ul>
+            <li>
+                <i class="fas fa-magnifying-glass"></i>
+            </li>
+            <li>
+                <span>Kids</span>
+            </li>
+            <li>
+                <i class="fas fa-bell"></i>
+            </li>
+            <li>
+                <span><i class="fas fa-user"></i>My account</span>
+            </li>
+        </ul>
         <input type="text" placeholder="Cerca qui il film o la serie tv" @keyup.enter="$emit('search')" v-model="this.store.endpoints.query">
     </div>
 </template>
